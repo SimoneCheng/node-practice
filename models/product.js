69ab439bc5c1ec8a341e0ae5d9da6a517c0f2f4e
SimoneCheng <bleach1827@gmail.com>
2022-09-01 23:08:39 +0800

    feature: adjust files

--- models/product.js
+++ models/product.js
@@ -1,34 +1,37 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
-    constructor(title, imageUrl, description, price) {
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price;
-    };
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-    save() {
-        const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
-        fs.readFile(p, (error, fileContent) => {
-            let products = [];
-            if (!error) {
-                products = JSON.parse(fileContent);
-            }
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (error) => { console.log(error); });
-        });
-    }
+  save() {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-    static fetchAll(callback) {
-        const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
-        fs.readFile(p, (error, fileContent) => {
-            if (error) {
-                callback([]);
-            } else {
-                callback(JSON.parse(fileContent));
-            }
-        });
-    }
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
 };
