4497640a226461a6bcf7d95edccbe46368d2efc6
SimoneCheng <bleach1827@gmail.com>
2022-09-25 22:34:55 +0800

    feature: connecting to database

--- utils/database.js
+++ utils/database.js
@@ -1,3 +1,12 @@
+require('dotenv').config();
+
 const mysql = require('mysql2');
 
-const pool = mysql.creat
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB,
+    password: process.env.DB_PASS,
+});
+
+module.exports = pool.promise();
