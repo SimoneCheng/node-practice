5c10d93150fc429bc123720fedc5dbb058b30c59
SimoneCheng <bleach1827@gmail.com>
2022-11-29 22:21:27 +0800

    feature: practicing session

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
+const session = require('express-session');
 
 const errorController = require("./controllers/error");
 
@@ -24,6 +25,7 @@ app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false}))
 
 app.use((req, res, next) => {
   User.findByPk(1)
